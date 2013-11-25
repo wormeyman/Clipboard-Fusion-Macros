6ea76441ef93298aba1c84ee43478146303c4374
wormeyman <wormeyman@gmail.com>
2013-11-25 13:03:30 -0800

    Fixed tabbing

--- Remove-Characters.cs
+++ Remove-Characters.cs
@@ -4,11 +4,11 @@ public static class ClipboardFusionHelper
 {
 	public static string ProcessText(string text)
 	{
-			 return text.Replace(" ", "-")					//space
+			 return text.Replace(" ", "-")			//space
 						.Replace(":","-")			//colon
 						.Replace(";","-")			//semi-colon
-						.Replace("&","-")   			//Ampersand
-						.Replace("%","-")   			//Percent
+						.Replace("&","-")			//Ampersand
+						.Replace("%","-")			//Percent
 						.Replace("?","-")			//QuestionMark
 						.Replace("�","-")			//Em dash
 						.Replace("�","-")			//En dash (kindof redundant)
