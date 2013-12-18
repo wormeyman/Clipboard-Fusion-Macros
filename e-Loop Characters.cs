bd84d777f533c7c25368ef5ecca005ccc2ae2e3e
wormeyman <wormeyman@gmail.com>
2013-12-18 14:55:31 -0800

    Create e-Loop Characters.cs

---
+++ e-Loop Characters.cs
@@ -0,0 +1,15 @@
+using System;
+
+public static class ClipboardFusionHelper
+{
+	public static string ProcessText(string text)
+	{
+			 return text.Replace("•", "&bull;")			//Bullet
+						.Replace("“","\"")				//Smart Quote
+						.Replace("”","\"")				//Smart Quote
+						.Replace("’","'")				//Smart Quote
+						.Replace("—","&mdash;")			//Em dash
+						.Replace("https://","http://")	//ssl remover
+						.Replace("…","...");			//elipses remover
+	}
+}
