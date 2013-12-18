b6cb5f2689da3cae016dc2f4c3f605f6a3088062
wormeyman <wormeyman@gmail.com>
2013-12-18 14:57:47 -0800

    changed tabs to spaces

--- e-Loop Characters.cs
+++ e-Loop Characters.cs
@@ -2,14 +2,14 @@ using System;
 
 public static class ClipboardFusionHelper
 {
-	public static string ProcessText(string text)
-	{
-			 return text.Replace("•", "&bull;")			//Bullet
-						.Replace("“","\"")				//Smart Quote
-						.Replace("”","\"")				//Smart Quote
-						.Replace("’","'")				//Smart Quote
-						.Replace("—","&mdash;")			//Em dash
-						.Replace("https://","http://")	//ssl remover
-						.Replace("…","...");			//elipses remover
-	}
+    public static string ProcessText(string text)
+    {
+ return text.Replace("•", "&bull;")                     //Bullet
+                        .Replace("“","\"")              //Smart Quote 
+                        .Replace("”","\"")              //Smart Quote
+                        .Replace("’","'")               //Smart Quote
+                        .Replace("—","&mdash;")         //Em dash
+                        .Replace("https://","http://")  //ssl remover
+                        .Replace("…","...");            //elipses remover
+    }
 }
