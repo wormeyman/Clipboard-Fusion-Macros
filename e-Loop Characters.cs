f17e98803d97805fa67e2ccd72bd197be0df0ed6
wormeyman <wormeyman@gmail.com>
2015-02-24 12:33:35 -0800

    One tab for comment

--- e-Loop Characters.cs
+++ e-Loop Characters.cs
@@ -4,12 +4,12 @@ public static class ClipboardFusionHelper
 {
     public static string ProcessText(string text)
     {
-             return text.Replace("•", "&bull;")         //Bullet
-                        .Replace("“","\"")              //Smart Quote
-                        .Replace("”","\"")              //Smart Quote
-                        .Replace("’","'")               //Smart Quote
-                        .Replace("—","&mdash;")         //Em dash
-                        .Replace("https://","http://")  //ssl remover
-                        .Replace("…","...");            //elipses remover
+             return text.Replace("•", "&bull;")           //Bullet
+                        .Replace("“","\"")                //Smart Quote
+                        .Replace("”","\"")                //Smart Quote
+                        .Replace("’","'")                 //Smart Quote
+                        .Replace("—","&mdash;")           //Em dash
+                        .Replace("https://","http://")    //ssl remover
+                        .Replace("…","...");              //elipses remover
     }
 }
