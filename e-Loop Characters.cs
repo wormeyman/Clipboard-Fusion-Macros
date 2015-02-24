8753f2c4a105333edd634434639c1c6ebbd45483
wormeyman <wormeyman@gmail.com>
2015-02-24 12:32:48 -0800

    Remove Trailing Space

--- e-Loop Characters.cs
+++ e-Loop Characters.cs
@@ -5,7 +5,7 @@ public static class ClipboardFusionHelper
     public static string ProcessText(string text)
     {
  return text.Replace("•", "&bull;")                     //Bullet
-                        .Replace("“","\"")              //Smart Quote 
+                        .Replace("“","\"")              //Smart Quote
                         .Replace("”","\"")              //Smart Quote
                         .Replace("’","'")               //Smart Quote
                         .Replace("—","&mdash;")         //Em dash
